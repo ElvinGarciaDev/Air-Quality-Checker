763b265a460c0d76e2db5a4702e633f6e61be1cf
Elvin Garcia <elvingarcia19@gmail.com>
2023-06-13 18:45:04 -0400

    finishing getAirQuality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import "./App.css";
+
+const getAirQuality = async (city) => {
+  try {
+    const response = await fetch(
+      `https://api.waqi.info/feed/${city}/?token=${process.env.REACT_APP_AQI_API_TOKEN}`
+    );
+    const data = await response.json();
+
+    console.log(data);
+  } catch (error) {
+    console.error("network error:", error);
+
+    //set error state
+    //sset air quality data to null
+  }
+};
 
 function App() {
-  return (
-    <h1>Hello World</h1>
-  );
+  return <h1>Hello World</h1>;
 }
 
 export default App;
